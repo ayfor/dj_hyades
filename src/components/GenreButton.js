843f778f187affa24e71fa8922be2d449c60e82f
ayfor <josh.a.stubbington@gmail.com>
2021-11-01 20:28:58 -0400

    added icons

--- src/components/GenreButton.js
+++ src/components/GenreButton.js
@@ -1,8 +1,12 @@
+import { FaCheck } from "react-icons/fa"
+
 function GenreButton({ genre }) {
 
     return (
         <div className="w-min py-2 px-4 font-semibold rounded-lg shadow-md outline-green text-green-400 hover:text-white hover:bg-green-400 active:bg-green-700 cursor-pointer">
-            { genre }
+            <div className="flex flex-row justify-center items-center">
+                <FaCheck/> <div className="pl-3">{ genre }</div>
+            </div>
         </div>
     );
 }
