bb87648dbff6661d58748fe6068df794accb87e9
ayfor <josh.a.stubbington@gmail.com>
2022-01-20 23:22:56 -0500

    Added local list of genres, component for GenreSection and random selection of genres

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import Navbar from './Navbar'
-import GenreButton from './GenreButton'
+import GenreSelection from './GenreSelect/GenreSection';
+import GenreButton from './GenreSelect/GenreButton'
 
 function App() {
   return (
     <div className="App">
         <Navbar />
-        <div className="container container-main py-5">'
+        <div className="container container-main py-5">
           <div className="flex flex-col items-center">
+            <GenreSelection/>
             <GenreButton genre="Test" enabled ="true"/>
           </div>
         </div>
---
+++ src/components/GenreSelect/GenreSection.js
@@ -0,0 +1,30 @@
+import GenreButton from './GenreButton';
+
+const { genres } = require('./genres.json');
+
+const GenreSelection = () => {
+
+    let genreSubset = [];
+
+    //Randomly select a given number of genres from the 'genres' array
+    for (let index = 0; index < 15; index++) {
+
+        const randomIndex = Math.floor(Math.random() * (genres.length - 0 + 1) + 0);
+        const randomlySelectedGenre = genres[randomIndex];
+
+        genreSubset.push(randomlySelectedGenre);
+        
+    }
+
+    return ( 
+        <div className='container flex flex-row justify-center items-center flex-wrap'>
+            {
+                genreSubset.map((genre) => (
+                    <GenreButton key={genre} genre={genre} />
+                ))
+            }
+        </div>
+     );
+}
+ 
+export default GenreSelection;
