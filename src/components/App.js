297cd6df056b4e5a7b84c0be63659d8143fe922a
ayfor <josh.a.stubbington@gmail.com>
2022-06-08 22:28:15 -0400

    removed navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,11 @@
 import "./App.css";
-import Navbar from "./Navbar";
 import PlaylistBrowser from "./pages/PlaylistBrowser";
 import ThemeMapper from "./data/ThemeMapper";
-
 
 function App() {
 	return (
 		<ThemeMapper>
 			<div className="App">
-				<Navbar />
 				<PlaylistBrowser />
 			</div>
 		</ThemeMapper>
