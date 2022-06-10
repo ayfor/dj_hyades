79de3f4833eed8a24df216072f5393428ffdf951
ayfor <josh.a.stubbington@gmail.com>
2022-06-10 00:43:18 -0400

    first pass on spotify auth

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,45 @@
 import "./App.css";
+import { useEffect, useState } from "react";
 import PlaylistBrowser from "./pages/PlaylistBrowser";
 import ThemeMapper from "./data/ThemeMapper";
 
 function App() {
+	const [token, setToken] = useState("");
+	const CLIENT_ID = "84d9870e8c0a430d946fa11ae9afb055";
+	const REDIRECT_URI = "http://localhost:3000";
+	const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
+	const RESPONSE_TYPE = "token";
+
+	useEffect(() => {
+		const hash = window.location.hash;
+		let token = window.localStorage.getItem("token");
+
+		if (!token && hash) {
+			token = hash
+				.substring(1)
+				.split("&")
+				.find((elem) => elem.startsWith("access_token"))
+				.split("=")[1];
+
+			window.location.hash = "";
+			window.localStorage.setItem("token", token);
+		}
+
+		setToken(token);
+	}, []);
+
+	const logout = () => {
+		setToken("");
+		window.localStorage.removeItem("token");
+	};
+
 	return (
 		<ThemeMapper>
+			{!token ? (
+				<a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login to Spotify</a>
+			) : (
+				<button onClick={logout}>Logout</button>
+			)}
 			<div className="App">
 				<PlaylistBrowser />
 			</div>
