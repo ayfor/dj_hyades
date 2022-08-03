f0184520b5c83c7fefc875e616412976a7187c1b
ayfor <josh.a.stubbington@gmail.com>
2022-08-02 23:10:11 -0400

    added better cards for playlists

--- src/components/Playlist/PlaylistBrowser.js
+++ src/components/Playlist/PlaylistBrowser.js
@@ -34,22 +34,34 @@ const PlaylistBrowser = ({ playlists }) => {
 	return (
 		<div className="container container-main py-5">
 			<div className="flex flex-col items-center">
-				<h1 className="text-3xl font-bold text-center">Available Playlists</h1>
+				<h1 className="text-3xl font-bold text-center">Playlists</h1>
 				{availablePlaylists.length > 0 ? (
 					<>
 						{availablePlaylists.map((playlist) => {
 							return (
-								<div className="py-3">
-									<div className="overflow-hidden shadow-lg rounded-2xl h-90 w-64 m-auto">
-										<img alt="playlist preview image" src={playlist.imageUrl} className="rounded-t-lg" />
-										<div className="bg-white w-full p-4 relative">
-											<button aria-label="Go to article" type="button" className="absolute rounded-full bg-indigo-500 text-white w-12 h-12 right-8 -top-6">
-												<svg width="20" height="20" fill="currentColor" className="h-6 w-6 text-white mx-auto" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
-													<path d="M491 1536l91-91-235-235-91 91v107h128v128h107zm523-928q0-22-22-22-10 0-17 7l-542 542q-7 7-7 17 0 22 22 22 10 0 17-7l542-542q7-7 7-17zm-54-192l416 416-832 832h-416v-416zm683 96q0 53-37 90l-166 166-416-416 166-165q36-38 90-38 53 0 91 38l235 234q37 39 37 91z"></path>
+								<div className="py-3" key={playlist.id}>
+									<div className="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+										<a href="#">
+											<img className="rounded-t-lg" src={playlist.imageUrl} alt="" />
+										</a>
+										<div className="p-5">
+											<a href="#">
+												<h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{playlist.name}</h5>
+											</a>
+											<p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{playlist.description}</p>
+											<a
+												href="#"
+												className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+											>
+												Read more
+												<svg aria-hidden="true" className="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+													<path
+														fill-rule="evenodd"
+														d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+														clip-rule="evenodd"
+													></path>
 												</svg>
-											</button>
-											<p className="text-gray-800 text-xl font-medium mb-2">{playlist.name}</p>
-											<p className="text-gray-600 text-xs">{playlist.description}</p>
+											</a>
 										</div>
 									</div>
 								</div>
